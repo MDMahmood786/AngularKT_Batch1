2c171ea92a1bbdd8158f759287185be36dccee9b
nagabhushanamn <nagabhushanamn@gmail.com>
2016-11-22 11:34:36 +0530

    js concepts

---
+++ JS-learn/js-quiz-4.js
@@ -0,0 +1,15 @@
+
+
+function Person(name,age) {
+    this.name = name;
+    this.age = age;
+   
+    var self = this;
+    setInterval(function () { 
+        self.age++;
+        console.log(self.age);
+    },1000);    
+}
+
+
+let p=new Person("Ria",0);
